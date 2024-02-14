4e468ab833198893a5223876c0f6920f8c16bfbc
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2024-02-14 00:23:10 -0600

    upd eval

--- FM_Partition/eval.sh
+++ FM_Partition/eval.sh
@@ -1,12 +1,15 @@
-MY_DIR='output/ch48458'
-REF_DIR='output/reference'
+MY_DIR="output/ch48458"
+REF_DIR="output/reference"
 
-
-for i in {0..10}
+for i in {0..20}
 do
-  CASE='example_$i.txt'
+  CASE="example_$i.txt"
   diff ${MY_DIR}/${CASE} ${REF_DIR}/${CASE}
   ret=$?
-  if [ ret 
+  if [ $ret -eq 0 ]; then
+    echo "[Info] ${CASE} same! :)"
+  else
+    echo "[Error] ${CASE} different.. :("
+  fi
 done
 
